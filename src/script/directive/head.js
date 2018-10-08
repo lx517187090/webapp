4ae67566989ac6fb4c3603ec0d9711115e19a76e
lx517187090 <517187090@qq.com>
2018-10-08 22:23:38 +0800

    zd

---
+++ src/script/directive/head.js
@@ -0,0 +1,10 @@
+'user strict'
+
+angular.module('app')
+    .directive('appHead',[function () {
+        return{
+            restrict: 'A',
+            replace: true,
+            templateUrl:'/view/template/head.html'
+        };
+    }])
